96a50d53fcfc6e6ae1f162066503e7eed5a3f4ac
namlephuong95 <namlephuong95@gmail.com>
2020-06-20 16:47:52 +0700

    1.2-Lap-ghep-giao-dien-nguoi-dung-trang-ban-hang

--- FinalArtsShop/Controllers/TestThemeController.cs
+++ FinalArtsShop/Controllers/TestThemeController.cs
@@ -24,5 +24,44 @@ namespace FinalArtsShop.Controllers
             return View("~/Views/Backend/DemoDatatable.cshtml");
         }
 
+        public ActionResult IndexFrontend()
+        {
+            return View("~/Views/Frontend/Home.cshtml");
+        }
+
+        public ActionResult ProductFrontend()
+        {
+            return View("~/Views/Frontend/Product.cshtml");
+        }
+
+        public ActionResult ProductDetailFrontend()
+        {
+            return View("~/Views/Frontend/ProductDetail.cshtml");
+        }
+
+        public ActionResult CartFrontend()
+        {
+            return View("~/Views/Frontend/Cart.cshtml");
+        }
+
+        public ActionResult CheckoutFrontend()
+        {
+            return View("~/Views/Frontend/Checkout.cshtml");
+        }
+
+        public ActionResult AccountFrontend()
+        {
+            return View("~/Views/Frontend/Account.cshtml");
+        }
+
+        public ActionResult ContactFrontend()
+        {
+            return View("~/Views/Frontend/Contact.cshtml");
+        }
+
+        public ActionResult WishListFrontend()
+        {
+            return View("~/Views/Frontend/WishList.cshtml");
+        }
     }
 }

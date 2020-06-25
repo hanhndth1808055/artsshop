661245d672998012d25ecf6afcc7f859fba284e9
hanhndth1808055 <nguyendoanhanh.ulis@gmail.com>
2020-06-25 20:47:47 +0700

    Feature#11.1. And 10.4. Them gio hang, them san pham vao gio hang

--- FinalArtsShop/Controllers/HomeController.cs
+++ FinalArtsShop/Controllers/HomeController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FinalArtsShop.Models;
 
 namespace FinalArtsShop.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        private ApplicationDbContext db = new ApplicationDbContext();
 
+        // public ShoppingCart shoppingCart = new ShoppingCart();
+
+        private const string ShoppingCartName = "ShoppingCartName";
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,11 +21,48 @@ namespace FinalArtsShop.Controllers
             return View();
         }
 
+        public ActionResult Index()
+        {
+            var keyChainCategory = db.Categories.Where(c => c.Abbreviation == "KC").FirstOrDefault();
+
+            var products = db.Products.Where(p => p.CategoryID == keyChainCategory.Id).ToList();
+
+            return View("~/Views/Home/Home.cshtml", products);
+        }
+
+        public ActionResult Product()
+        {
+            return View("~/Views/Home/Product.cshtml");
+        }
+
+        public ActionResult ProductDetail()
+        {
+            return View("~/Views/Home/ProductDetail.cshtml");
+        }
+
+        public ActionResult Cart()
+        {
+            return View("~/Views/Home/Cart.cshtml");
+        }
+
+        public ActionResult Checkout()
+        {
+            return View("~/Views/Home/Checkout.cshtml");
+        }
+
+        public ActionResult Account()
+        {
+            return View("~/Views/Home/Account.cshtml");
+        }
+
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View("~/Views/Home/Contact.cshtml");
+        }
 
-            return View();
+        public ActionResult WishList()
+        {
+            return View("~/Views/Home/WishList.cshtml");
         }
     }
 }

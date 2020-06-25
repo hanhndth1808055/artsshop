70d06e0c8022afe57f14780475880934dc6375ee
namlephuong95 <namlephuong95@gmail.com>
2020-06-26 06:49:14 +0700

    10.1-Hien-thi-danh-sach-san-pham-o-trang-chu

---
+++ FinalArtsShop/Models/ViewShoppingCart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalArtsShop.Models
+{
+    public class ViewShoppingCart
+    {
+        public List<Category> CategoriesMenu { get; set; }
+        public ShoppingCart shoppingCart { get; set; }
+
+    }
+}

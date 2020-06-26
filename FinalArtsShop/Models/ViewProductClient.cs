bc4358dee95b9c6a4951821767ef232c35ea07be
namlephuong95 <namlephuong95@gmail.com>
2020-06-26 18:49:09 +0700

    10.3-Hien-thi-danh-sach-san-pham-o-trang-Chi-tiet-san-pham

---
+++ FinalArtsShop/Models/ViewProductClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalArtsShop.Models
+{
+    public class ViewProductClient
+    {
+        public List<Category> CategoriesMenu { get; set; }
+        public List<Product> LatestProducts { get; set; }
+        public Product Product { get; set; }
+    }
+}

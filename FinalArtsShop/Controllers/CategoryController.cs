2b0c54ad84eb59bd56e821875f7128c8da00b954
namlephuong95 <namlephuong95@gmail.com>
2020-06-26 17:58:14 +0700

    10.2-Hien-thi-danh-sach-san-pham-trang-Categories-(-chua-filter-va-panginate-)

---
+++ FinalArtsShop/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using FinalArtsShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalArtsShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Category
+        public ActionResult Index(int id)
+        {
+            ViewCategoryClient viewCategoryClient;
+
+            var CategoriesMenu = db.Categories.Where(c => c.Active == 1).ToList();
+            var products = db.Products.AsQueryable();
+
+            if (id != null && id != 0)
+            {
+                var cate = db.Categories.Where(c => c.Id == id && c.Active == 1).FirstOrDefault();
+                ViewBag.CateName = cate.Name;
+                if (cate.Parent == 0)
+                {
+                    products = products.Where(p => p.isActive == 1 && p.Category.Parent == id).Take(15);
+                }
+                else
+                {
+                    products = products.Where(p => p.isActive == 1 && p.CategoryID == id).Take(15);
+                }
+            }
+            else
+            {
+
+            }
+
+            viewCategoryClient = new ViewCategoryClient() { 
+                CategoriesMenu = CategoriesMenu,
+                Products = products.ToList(),
+                FeatureProducts = db.Products.Where(p => p.isFeature > 0).Take(3).ToList()
+            };
+
+            return View(viewCategoryClient);
+        }
+    }
+}
---
+++ FinalArtsShop/Models/ViewCategoryClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalArtsShop.Models
+{
+    public class ViewCategoryClient
+    {
+        public List<Category> CategoriesMenu { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Product> FeatureProducts { get; set; }
+        public List<Product> LatestProducts { get; set; }
+    }
+}

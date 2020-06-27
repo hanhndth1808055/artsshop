741d940275b51fb00db22cbf5d59400805f325c8
namlephuong95 <namlephuong95@gmail.com>
2020-06-27 14:56:08 +0700

    Feature#10.6. Show thong tin cart

---
+++ FinalArtsShop/Controllers/CheckoutController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalArtsShop.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace FinalArtsShop.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+;
+        // GET: Checkout
+        public ActionResult Index()
+        {
+            var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            ApplicationUser user = userManager.FindByNameAsync(User.Identity.Name).Result;
+            ViewCheckoutClient viewCheckoutClient = new ViewCheckoutClient() {
+                User = user,
+                Cities = db.Cities.ToList(),
+                Districts = db.Districts.ToList()
+            };
+            return View(viewCheckoutClient);
+        }
+    }
+}
---
+++ FinalArtsShop/Models/ViewCheckoutClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalArtsShop.Models
+{
+    public class ViewCheckoutClient
+    {
+        public List<City> Cities { get; set; }
+        public List<District> Districts { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
